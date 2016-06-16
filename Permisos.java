a03ef3143b0b9e0fbc2ea35ae9e3a1a5082537f4
Omar Lenin Reyes Alonso <OMAR94.OLR@GMAIL.COM>
2016-06-16 11:59:44 -0500

    a
    a

---
+++ Permisos.java
@@ -0,0 +1,49 @@
+package com.android.lenin.contentprovidersejemplo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NuevoContactoActivity extends AppCompatActivity {
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_contact_layout);
+        PedirPermiso();
+       
+    }
+    private void PedirPermiso() {
+        int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_CONTACTS);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.WRITE_CONTACTS},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // OK
+					   Toast.makeText(this, "Permiso Obtenido", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    // Denegado
+                    Toast.makeText(this, "No se obtuvo el permiso", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
